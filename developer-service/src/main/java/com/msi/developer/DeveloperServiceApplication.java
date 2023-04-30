d96fa7f78ebdaaa69097fc46dc088c0dfdfad032
Guy Biton-XGQ376 <guy.biton@motorolasolutions.com>
2023-04-30 09:39:11 +0300

    add liquibase

--- developer-service/src/main/java/com/msi/developer/DeveloperServiceApplication.java
+++ developer-service/src/main/java/com/msi/developer/DeveloperServiceApplication.java
@@ -3,8 +3,11 @@ package com.msi.developer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import liquibase.integration.spring.SpringLiquibase;
 
 @SpringBootApplication
 @ComponentScan({"com.msi.developer.controller", "com.msi.developer.service"})
@@ -15,5 +18,4 @@ public class DeveloperServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeveloperServiceApplication.class, args);
 	}
-
 }
---
+++ developer-service/src/main/java/com/msi/developer/LiquibaseConfiguration.java
@@ -0,0 +1,8 @@
+package com.msi.developer;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+}
