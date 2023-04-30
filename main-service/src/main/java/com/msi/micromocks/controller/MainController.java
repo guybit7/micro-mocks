adb8764319dd8bae59e13ca1126c1af4265951ec
Guy Biton-XGQ376 <guy.biton@motorolasolutions.com>
2023-04-30 15:04:38 +0300

    add config server

--- main-service/src/main/java/com/msi/micromocks/controller/MainController.java
+++ main-service/src/main/java/com/msi/micromocks/controller/MainController.java
@@ -3,6 +3,8 @@ package com.msi.micromocks.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,15 @@ import com.msi.micromocks.service.MocksService;
 
 @RestController
 @RequestMapping("/api/main")
+@RefreshScope
 public class MainController {
 	
 	@Autowired
 	MocksService mocksService;
-
+	
+	@Value("${address.test}")
+	private String test;
+	
 	public MainController() {
 		System.out.println("CTRL");		
 	}
@@ -33,4 +39,8 @@ public class MainController {
 		return mocksService.getByDeveloperId(id);
 	}
 
+	@GetMapping("/test")
+	public String test() {
+		return test;
+	}
 }
